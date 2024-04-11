1fd0bf44c3aab4444b3f4ea1443ad09b78cfa018
Ruben Otero <ruben87sp@gmail.com>
2024-04-11 19:04:59 +0200

    database connection

--- client.js
+++ client.js
@@ -2,12 +2,8 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
-});
+  connectionString: process.env.POSTGRES_URL ,
+})
 
 pool.query('SELECT NOW()', (err, res) => {
   if (err) {
